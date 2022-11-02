cde9dec16b377a7b502d02a03f5f9629b9e1db03
Selene Clark <michellc@byui.edu>
2022-11-02 15:44:55 -0600

    Initial Commit

---
+++ Routeens/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Routeens
+//
+//  Created by Selene on 11/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Routeens/RouteensApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteensApp.swift
+//  Routeens
+//
+//  Created by Selene on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct RouteensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
