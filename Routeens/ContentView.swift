d0a93c98b4a5d4065f3e85080740289ed033cafb
Selene Clark <michellc@byui.edu>
2022-11-02 16:04:44 -0600

    Starting project

--- Routeens/ContentView.swift
+++ Routeens/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+	   NavigationView {
+		  
+		  Text("Hello, world!")
+			  .padding()
+			  
+		  
+		  
+		  .navigationTitle("Routeens")
+	   }
+	   
+        
     }
 }
 
---
+++ Routeens/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  Routeens
+//
+//  Created by Selene on 11/2/22.
+//
+
+import Foundation
