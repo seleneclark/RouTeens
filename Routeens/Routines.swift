a296bae104c1f49257534371430e235a279e0a53
Selene Clark <michellc@byui.edu>
2022-11-10 15:28:48 -0700

    Saving and loading from UserDefaults complete

--- Routeens/Routines.swift
+++ Routeens/Routines.swift
@@ -16,11 +16,13 @@ class Routines: ObservableObject {
 
    
    func loadActiveRoutines(){
-	  // reset pending if current date is after the end of the routine lastOpened date is
-	  resetPendingTasks()
+	  
 	  
 	  allRoutines.forEach { singleroutine in
 		 //maybe put reset pending here, since I'm already going thru each routine
+		 // reset pending if current date is after the end of the routine lastOpened date is
+		 resetPendingTasks()
+		 
 		 
 		 let start = Calendar.current.dateComponents([.hour, .minute], from: singleroutine.startTime)
 		 let end = Calendar.current.dateComponents([.hour, .minute], from: singleroutine.endTime)
@@ -29,9 +31,8 @@ class Routines: ObservableObject {
 			self.routine.append(singleroutine)
 			noPendingTasks = false
 		 }
-		 
 	  }
-	  
+	  save()
    }
    
    func resetPendingTasks(){
@@ -41,22 +42,36 @@ class Routines: ObservableObject {
    }
    
    init(){
+	  if let data = UserDefaults.standard.data(forKey: "SavedData"){
+		 if let decoded = try? JSONDecoder().decode([Routine].self, from: data){
+			allRoutines = decoded
+			return
+		 }
+	  }
 	  //I just want to load up the app with some tasks and routines without any data entry
-	  var morningTasks = [Task]()
-	  morningTasks.append(Task(name:"Brush teeth", pending: true))
-	  morningTasks.append(Task(name:"Make bed", pending: true))
-	  morningTasks.append(Task(name:"Take out trash", pending: false))
-	  morningTasks.append(Task(name:"Put backpack in car", pending: true))
-  
-	  let morningRoutine = Routine(routineName: "Morning Routine", tasks: morningTasks, startTimeHour: 6, startTimeMin: 0, endTimeHour: 12, endTimeMinute: 0)
-	  self.allRoutines.append(morningRoutine)
-	  var afternoonTasks = [Task]()
-	  afternoonTasks.append(Task(name:"Eat lunch", pending: true))
-	  afternoonTasks.append(Task(name:"Walk dog", pending: true))
-	  afternoonTasks.append(Task(name:"Empty Dishwasher", pending: false))
-  
-	  let afternoonRoutine = Routine(routineName: "Afternoon Routine", tasks: afternoonTasks, startTimeHour: 6, startTimeMin: 0, endTimeHour: 23, endTimeMinute: 0)
-	  self.allRoutines.append(afternoonRoutine)
+//	  var morningTasks = [Task]()
+//	  morningTasks.append(Task(name:"Brush teeth", pending: true))
+//	  morningTasks.append(Task(name:"Make bed", pending: true))
+//	  morningTasks.append(Task(name:"Take out trash", pending: false))
+//	  morningTasks.append(Task(name:"Put backpack in car", pending: true))
+//
+//	  let morningRoutine = Routine(routineName: "Morning Routine", tasks: morningTasks, startTimeHour: 6, startTimeMin: 0, endTimeHour: 12, endTimeMinute: 0)
+//	  self.allRoutines.append(morningRoutine)
+//	  var afternoonTasks = [Task]()
+//	  afternoonTasks.append(Task(name:"Eat lunch", pending: true))
+//	  afternoonTasks.append(Task(name:"Walk dog", pending: true))
+//	  afternoonTasks.append(Task(name:"Empty Dishwasher", pending: false))
+//
+//	  let afternoonRoutine = Routine(routineName: "Afternoon Routine", tasks: afternoonTasks, startTimeHour: 6, startTimeMin: 0, endTimeHour: 23, endTimeMinute: 0)
+//	  self.allRoutines.append(afternoonRoutine)
+	  allRoutines = []
+   }
+   
+   func save() {
+	  print("Got to save")
+	  if let encoded = try? JSONEncoder().encode(allRoutines) {
+		 UserDefaults.standard.set(encoded, forKey: "SavedData")
+	  }
    }
 }
 extension DateComponents: Comparable {
